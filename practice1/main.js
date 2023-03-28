d53c141e9c9bf16af454dc4841ed68be7c0839d8
Badr Moslih <113358493+Moslihbadr@users.noreply.github.com>
2023-03-29 00:05:01 +0200

    Add files via upload

---
+++ practice1/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  const t1 = "this paragraph is for the DESING button this paragraph is for the DESING button this paragraph is for the DESING button this paragraph is for the DESING button this paragraph is for the DESING button this paragraph is for the DESING button this paragraph is for the DESING button"
+  const t2 = "this paragraph is for the WEB button this paragraph is for the WEB button this paragraph is for the WEB button this paragraph is for the WEB button this paragraph is for the WEB button this paragraph is for the WEB button this paragraph is for the WEB button"
+  const t3 = "this paragraph is for the GRAPHIC button this paragraph is for the GRAPHIC button this paragraph is for the GRAPHIC button this paragraph is for the GRAPHIC button this paragraph is for the GRAPHIC button this paragraph is for the GRAPHIC button this paragraph is for the GRAPHIC button"
+  const t4 = "this paragraph is for the INSTRUCTOR button this paragraph is for the INSTRUCTOR button this paragraph is for the INSTRUCTOR button this paragraph is for the INSTRUCTOR button this paragraph is for the INSTRUCTOR button this paragraph is for the INSTRUCTOR button this paragraph is for the INSTRUCTOR button"
+  $(".1").on('click', ()=>{
+    $(".h6").text(t1).fadeToggle()
+  })
+  $(".2").on('click', ()=>{
+    $(".h6").text(t2)
+  })
+  $(".3").on('click', ()=>{
+    $(".h6").text(t3)
+  })
+  $(".4").on('click', ()=>{
+    $(".h6").text(t4)
+  })
+})
